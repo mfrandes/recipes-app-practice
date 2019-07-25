2d34c7037fa1c3d2a0f502352597ceeafc738d50
mfrandes <frandesmyrce@gmail.com>
2019-07-25 18:22:57 +0300

    Using com. and creating Recipe model

---
+++ src/app/recipes/recipes.model.ts
@@ -0,0 +1,12 @@
+export class Recipes{
+    public name: string;
+    public imagePath: string;
+    public description: string;
+
+    
+    constructor(name: string, imgPath, desc: string){
+        this.name = name;
+        this.imagePath = imgPath;
+        this.description= desc;
+    }
+}
